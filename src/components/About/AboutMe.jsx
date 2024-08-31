9d74192fed3c58a467f4b8a9fe2e8e45642f9349
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-31 13:34:27 +0530

    client updated

--- src/components/About/AboutMe.jsx
+++ src/components/About/AboutMe.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import aboutImage from '../../assets/Bonnie_image.jpg'; // Replace with the correct path to your image
-import topImage from '../../assets/mathias-reding-cmyDyDa6Wfk-unsplash.jpg'; // New image at the top
 import clientData from '../../clientData'; // Import the client data
 
 const AboutMe = () => {
@@ -13,7 +11,7 @@ const AboutMe = () => {
     <section className="w-full">
       {/* Top Image Section */}
       <header className="relative h-64 mb-12">
-        <img src={topImage} alt="Blog Header" className="w-full h-full object-cover" />
+        <img src={clientData.topImageUrl} alt="Blog Header" className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <div className="text-center text-white">
             <h1 className="text-5xl">About</h1>
@@ -25,15 +23,13 @@ const AboutMe = () => {
       {/* Two-Part Layout Below */}
       <div className="flex flex-col md:flex-row w-full mb-20">
         <div className="md:w-1/2 h-1/2 md:h-auto p-12">
-          <img src={aboutImage} alt="About Us" className="object-cover w-full h-full" />
+          <img src={clientData.dp} alt="About Us" className="object-cover w-full h-full" />
         </div>
         <div className="md:w-1/2 h-auto bg-white flex flex-col justify-center p-8 md:p-16">
-          {/* AboutMe Detailed Start */}
           <h2 className="text-3xl md:text-4xl font-bold mb-4">{clientData.agentName}</h2>
           <p className="text-gray-600">
             {clientData.aboutMeDetailed}
           </p>
-          {/* AboutMe Detailed End */}
         </div>
       </div>
     </section>
--- src/components/Home/AboutInfo.jsx
+++ src/components/Home/AboutInfo.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import dp from '../../assets/Bonnie_image.jpg';
 import clientData from '../../clientData'; // Import clientData
 
 const AboutInfo = () => {
   return (
     <div className="bg-white mt-12 py-16 px-8 flex flex-col lg:flex-row items-center lg:items-start justify-between">
-      {/* Left Section */}
       <div className="lg:w-1/2 w-60 mb-8 lg:mb-0 text-center lg:text-left">
         <h2 className="text-3xl lg:text-4xl font-bold text-gray-800 mt-10">
-          {/* AboutMe Short Title */}
           {clientData.heroTagline}
         </h2>
         <h3 className="text-xl lg:text-1xl text-gray-600 mt-4">
           Trusted Real Estate Advisor
         </h3>
         <p className="mt-6 text-lg text-gray-600 max-w-xl">
-          {/* AboutMe Short Description */}
           {clientData.aboutMeShort}
         </p>
         <Link to="/about" className="text-white hover:text-gray-300 relative group">
@@ -26,10 +22,8 @@ const AboutInfo = () => {
         </Link>
       </div>
 
-      {/* Right Section */}
       <div className="lg:w-1/2 w-full mt-6">
-        {/* Add a relevant image for the client */}
-        <img src={dp} alt="About" className="w-full h-auto object-cover" />
+        <img src={clientData.dp} alt="About" className="w-full h-auto object-cover" />
       </div>
     </div>
   );
