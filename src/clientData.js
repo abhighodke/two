b230dee8e16d72584dae23df1746dc296e36f7f0
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:36:53 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "KT",
-    heroTagline: "Kayla Tinney Realty",
-    agentName: "Kayla Tinney",
+    headerInitials: "GO",
+    heroTagline: "GO Realty",
+    agentName: "Greg Oliver",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Kayla Tinney was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Kayla Tinney provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Kayla Tinney were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Kayla Tinney and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Kayla Tinney is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Greg Oliver was a game-changer for us. Him deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Greg Oliver provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Greg Oliver were evident throughout our home-buying journey. Him ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Greg Oliver and Him team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Greg Oliver is the one. Him dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "",
-    address: "Talladega, Al, USA",
-    phone: "256-454-4471",
-    footerDescription: `Kayla Tinney Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "greg@gorealtyllc.net",
+    address: "296 E Main St, Centre, AL, United States",
+    phone: "256-504-3847",
+    footerDescription: `GO Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/KaylaTinneyReal/",
+        facebook: "https://www.facebook.com/gregolivergorealty",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
-        phone: "256-454-4471",
+        email: "greg@gorealtyllc.net",
+        phone: "256-504-3847",
     },
     website: "",
-    dp: "https://scontent.fpnq17-1.fna.fbcdn.net/v/t39.30808-6/417562319_7206530449411266_4149389413086778424_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=zPV2azcXKV0Q7kNvgEeBOPu&_nc_ht=scontent.fpnq17-1.fna&oh=00_AYBhlBH8EHnHVvcYuoMoPh_SymGvtOBBrOOTwznQb6-78g&oe=66D7AFD8" // Agent image URL
+    dp: "https://imagescdn.homes.com/i2/zeFbbnBuxdjMXuaxqmbAI00cYJXsoo9cytSfD6K8Ma0/117/greg-oliver.jpg" // Agent image URL
 };
 
 export default clientData;
